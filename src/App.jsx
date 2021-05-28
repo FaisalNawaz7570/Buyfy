1de55f379fbdc387ea3dab1fa9b54ce6d2b7521c
faisal nawaz saifi <faisalnawaz7570@gmail.com>
2021-05-28 19:46:41 +0500

    route level optimization code spliting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Authentication from "./Pages/Authentication/Authentication";
-import Category from "./Pages/Category/Category";
-import CategoryProduct from "./Pages/CategoryProducts/CategoryProducts";
-import Checkout from "./Pages/Checkout/Checkout";
-import { useEffect } from "react";
+// import Home from "./Pages/Home/Home";
+// import Authentication from "./Pages/Authentication/Authentication";
+// import Category from "./Pages/Category/Category";
+// import CategoryProduct from "./Pages/CategoryProducts/CategoryProducts";
+// import Checkout from "./Pages/Checkout/Checkout";
+import { useEffect, lazy, Suspense } from "react";
 import { connect } from "react-redux";
 import { firebaseAuthListener } from "./Redux/auth/authActions";
 import Navbar from "./Components/Navbar/Navbar";
-import Test from "./Pages/Test/Test";
-import Product from "./Pages/Product/Product";
+// import Test from "./Pages/Test/Test";
+// import Product from "./Pages/Product/Product";
+
+var Home = lazy(() => import("./Pages/Home/Home"));
+var Authentication = lazy(() =>
+  import("./Pages/Authentication/Authentication")
+);
+var Category = lazy(() => import("./Pages/Category/Category"));
+var CategoryProduct = lazy(() =>
+  import("./Pages/CategoryProducts/CategoryProducts")
+);
+var Product = lazy(() => import("./Pages/Product/Product"));
+var Test = lazy(() => import("./Pages/Test/Test"));
+var Checkout = lazy(() => import("./Pages/Checkout/Checkout"));
 
 function App({ firebaseAuthListener }) {
   useEffect(() => {
@@ -22,17 +34,19 @@ function App({ firebaseAuthListener }) {
       <Navbar />
       <Switch>
         <div className="pagesContainer">
-          <Route path="/" component={Home} exact />
-          <Route path="/authentication" component={Authentication} exact />
-          <Route path="/category" component={Category} exact />
-          <Route
-            path="/category-products/:category"
-            component={CategoryProduct}
-            exact
-          />
-          <Route path="/product/:productId" component={Product} exact />
-          <Route path="/checkout/:orderId" component={Checkout} exact />
-          <Route path="/test" component={Test} />
+          <Suspense fallback={<div>loading...</div>}>
+            <Route path="/" component={Home} exact />
+            <Route path="/authentication" component={Authentication} exact />
+            <Route path="/category" component={Category} exact />
+            <Route
+              path="/category-products/:category"
+              component={CategoryProduct}
+              exact
+            />
+            <Route path="/product/:productId" component={Product} exact />
+            <Route path="/checkout/:orderId" component={Checkout} exact />
+            <Route path="/test" component={Test} />
+          </Suspense>
         </div>
       </Switch>
     </div>
